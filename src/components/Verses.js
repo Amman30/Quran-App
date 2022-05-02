2551fc966898cd418bea3a76f8d93d00284c35d0
AMMAN <mhdammman@gmail.com>
2022-05-02 12:34:33 +0530

    Faced bug with audio component,disabled

--- src/components/Verses.js
+++ src/components/Verses.js
@@ -6,11 +6,12 @@ import "./Verses.css";
 const Verses = () => {
     const params = useParams();
     const [loading, setLoading] = useState(true);
-    const [verses, setVerses] = useState([]);
+    const [versess, setVersess] = useState([]);
     const [chapters, setChapters] = useState([]);
-    const audioRefs = useRef({});
-    const [versesWithAudio, setVersesWithAudio] = useState([]);
+    // const audioRefs = useRef({});
+    // const [versesWithAudio, setVersesWithAudio] = useState([]);
     const [showTranslation, setShowTranslation] = useState(true);
+    const [title, setTitle] = useState("Read Without Translation");
 
     useEffect(() => {
         fetch(
@@ -24,35 +25,42 @@ const Verses = () => {
             `https://api-scripture-iust-dev.herokuapp.com/v1/scripture/quraan/get?language=en&author=Ahmed%20Ali&text=simple&chapter=${params.id}`
         )
             .then(async (res) => {
-                setVerses((await res.json()).data);
+                setVersess((await res.json()).data);
                 setLoading(false);
             })
 
+        // fetch(`https://api.quran.sutanlab.id/surah/${params.id}`).then(
+        //     async (res) => {
+        //         setVersesWithAudio((await res.json()).data.verses);
+        //         setLoading(false);
+        //     }
+        // )
             .catch((error) => {
                 console.log(error);
             });
-
-        fetch(`https://api.quran.sutanlab.id/surah/${params.id}`).then(
-            async (res) => {
-                setVersesWithAudio((await res.json()).data.verses);
-                setLoading(false);
-            }
-        );
     }, [params.id]);
 
-    const playNextAudio = (currentIndex) => {
-        if (currentIndex < versesWithAudio.length - 1) {
-            audioRefs.current[currentIndex + 1].play();
-        }
-    };
+    // const playNextAudio = (currentIndex) => {
+    //     if (currentIndex < versesWithAudio.length - 1) {
+    //         audioRefs.current[currentIndex + 1].play();
+    //     }
+    // };
 
-    const pauseAllExcept = (currentIndex) => {
-        Object.keys(audioRefs.current).forEach((key) => {
-            if (parseInt(key) !== currentIndex) {
-                audioRefs.current[key].pause();
-            }
-        });
-    };
+
+    // const pauseAllExcept = (currentIndex) => {
+    //     Object.keys(audioRefs.current).forEach((key) => {
+    //         if (parseInt(key) !== currentIndex) {
+    //             audioRefs.current[key].pause();
+    //         }
+    //     });
+    // };
+
+    function abc() {
+        setTitle("Read With Translation");
+    }
+
+
+
 
     return loading ? (
         <Spinner />
@@ -60,41 +68,27 @@ const Verses = () => {
         <div className="verses">
             <br />
             <div className="reading">
-                {" "}
                 <button
                     style={{ color: "green" }}
-                    onClick={() => setShowTranslation(!showTranslation)}
+                    onClick={() => { setShowTranslation(!showTranslation); abc(); }}
+
                 >
-                    Read Without Translation
+                    {title}
                 </button>
             </div>
             <div className="bism"> ﷽ </div>
             <div className="surah">
-                {" "}
+
                 {chapters.map((chapter) => (
                     <div key={chapter._id}>
                         سورة &nbsp; {chapter.arabicName}
                     </div>
                 ))}
-            </div>{" "}
+            </div>
             <br />
-            {verses.map((verse) => (
+            {versess.map((verse) => (
                 <div key={verse.verse} className="text">
-                    <audio
-                        controls
-                        ref={(e) => (audioRefs.current[verse.verse] = e)}
-                        onPlay={() => pauseAllExcept(verse.verse)}
-                        onEnded={() => playNextAudio(verse.verse)}
-                    >
-                        <source
-                            src={
-                                versesWithAudio.find(
-                                    (v) => v.number.inSurah === verse.verse
-                                ).audio.primary
-                            }
-                            type="audio/mpeg"
-                        />
-                    </audio>
+
                     &nbsp;
                     <div className="right"> {verse.data.text} ۝ </div>
                     {showTranslation ? (
@@ -113,3 +107,32 @@ const Verses = () => {
 };
 
 export default Verses;
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div key={versess.inQuran} className="audios">
+                        <audio
+                            controls
+                            ref={(e) => (audioRefs.current[verse.verse] = e)}
+                            onPlay={() => pauseAllExcept(verse.verse)}
+                            onEnded={() => playNextAudio(verse.verse)}
+                        >
+                            <source
+                                src={
+                                    versesWithAudio.find(
+                                        (v) => v.number.inSurah === verse.verse
+                                    ).audio.primary
+                                }
+                                type="audio/mp3"
+                            />
+                        </audio>
+                    </div> */}
